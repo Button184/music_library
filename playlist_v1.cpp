572cbbe8adc1b16075eb83c93b631a7b1a5f5add
Eric Braun <erbrmt00@hs-esslingen.de>
2025-01-04 08:46:16 +0100

    update_testlauf

---
+++ playlist_v1.cpp
@@ -0,0 +1,11 @@
+// bei änderungen: git status -> git commit -am "update [beschreibung]"
+// -> git push origin m
+
+#include <iostream>
+
+int main() {
+    printf("test fuer git");
+
+    return 0;
+}
+
