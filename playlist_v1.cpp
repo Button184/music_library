8f88ebba9d2d0e551d8fb4b55d2baa96df8a390b
Eric Braun <erbrmt00@hs-esslingen.de>
2025-01-15 22:08:29 +0100

    lied entfernen funktion funktioniert

--- playlist_v1.cpp
+++ playlist_v1.cpp
@@ -109,12 +109,49 @@ string playlist = "C:/Users/Admin/Desktop/Code/VSC/swt_projekt_playlist/playlist
         datei <<"\n"<<neuesLied.artist <<";"<<neuesLied.songname<<";"<<neuesLied.genre<<";"<<neuesLied.duration<<";";
 
         datei.close();
-        cout<<"**Lied hinzugefuegt!**"<<endl;
+        cout<<"**Lied erfolgreich hinzugefuegt!**"<<endl;
         
     }
 
-    void LiedLoeschen() {
-        cout<<"function not yet build"<<endl;
+    void LiedLoeschen(const string playlist) {
+        string titel;
+
+        cout<<"Gebe den Songtitel ein, um das entsprechende Lied zu löschen.\n";
+        cin.ignore();
+        getline(cin, titel);
+
+        ifstream datei(playlist);
+        
+        if(!datei.is_open()) {
+            cerr << "!!Datei konnte nicht geoeffnet werden!!" <<endl;
+            return;
+        }
+
+        vector<string>lines;
+        string line; 
+        bool found = false;
+
+        while(getline(datei, line)) {
+            size_t pos = line.find(titel);
+            if(pos !=string::npos) {
+                cout<<line<< "Wird erntfernt..." <<endl;
+                found = true; continue;
+            }
+            lines.push_back(line);
+            
+        }
+        datei.close();
+
+        ofstream datei_neu(playlist, ios::trunc);
+        if (!datei_neu.is_open()) {
+            cerr <<"!!Datei konnte nicht erstellt werden!!"<< endl;
+            return;
+        }
+        for(const auto& l : lines) {
+            datei_neu << l << endl;
+        }
+        datei_neu.close();
+        cout<<"**Lied erfolgreich entfernt!**"<<endl;
 
     }
 
@@ -122,7 +159,6 @@ string playlist = "C:/Users/Admin/Desktop/Code/VSC/swt_projekt_playlist/playlist
         BibliothekEinlesen(playlist);
 
     }
-
     void BibliothekSpeichern() {
         cout<<"function not yet build"<<endl;
 
@@ -160,7 +196,7 @@ int main() {
                 LiedHinzufuegen(playlist);
                 break; }
             case 3:
-                LiedLoeschen();
+                LiedLoeschen(playlist);
                 break;
             case 4:
                 BibliothekAnzeigen(playlist);
